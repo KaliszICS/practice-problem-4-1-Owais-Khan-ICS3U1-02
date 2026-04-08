a13715b5eb8980ef444f1a6cd09247503dc83858
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-04-08 14:42:02 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,44 @@
+/**
+	* File: Lesson 4.1: While Loops and accumulators
+	* Author: Owais Ali Khan
+	*	Date Created: April 8, 2026
+	*	Date Last Modified: April 8, 2026
+	*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
-	public static void q1() {
-		//Write question 1 code here
+	public static String seriesInclusive(int n) {
+		if (n <= 0) {
+			return "";
+		}
+		
+		String result = "1";
+		int i = 1;
+		while (i < n) {
+			i++;
+			result = result + " " + i;
+		}
+		return result;
 	}
 
-	public static void q2() {
-		//Write question 2 code here
+	public static String seriesExclusive(int n) {
+		if (n <= 0) {
+			return "";
+		}
+
+		String result = "0";
+		int i = 0;
+		while (i < n-1) {
+			i++;
+			result = result + " " + i;
+		}
+		return result;
 	}
 
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
+	
 
 }
